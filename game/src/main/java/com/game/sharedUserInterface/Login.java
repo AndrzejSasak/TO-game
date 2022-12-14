e9c4609f81f54abbf6046c715817070b51bf2059
Tomek Stolarz <tomekstolarz0@gmail.com>
2022-12-14 21:32:27 +0100

    -[fix] fixed error after refactor

--- game/src/main/java/com/game/sharedUserInterface/Login.java
+++ game/src/main/java/com/game/sharedUserInterface/Login.java
@@ -2,20 +2,22 @@ package com.game.sharedUserInterface;
 
 import com.game.controllers.PlayerEntityController;
 import com.game.entities.Entity;
+import com.game.entities.User;
 
 import java.util.Scanner;
 
 public class Login {
     Entity entity;
-    PlayerEntityController playerEntityControllerController = new PlayerEntityController();
-
+    User user = new User();
+    PlayerEntityController playerEntityController = new PlayerEntityController();
 
     public Entity createPlayer() {
         Scanner inputReader = new Scanner(System.in);
         System.out.print("Type player login: ");
         String login = inputReader.nextLine();
-        playerEntityControllerController.setLogin(login);
-        entity = new SelectEntityType().createNewEntity(entity, playerEntityControllerController);
+        user.setLogin(login);
+        //TODO: store state of user in some file
+        entity = new SelectEntityType().createNewEntity(entity, playerEntityController);
         return entity;
     }
 }
