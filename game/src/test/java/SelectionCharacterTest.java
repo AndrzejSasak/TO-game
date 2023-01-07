10442ae60e859df5febbd5b173fb031b0dcd6ce8
Tomek Stolarz <tomekstolarz0@gmail.com>
2023-01-07 12:23:26 +0100

    -[feat] added selection character outputs tests

--- game/src/test/java/SelectionCharacterTest.java
+++ game/src/test/java/SelectionCharacterTest.java
@@ -4,8 +4,17 @@ import com.game.controllers.PlayerEntityController;
 import com.game.entities.Entity;
 import com.game.entities.User;
 import com.game.entitiesFactories.PlayerEntityFactory;
+import com.game.sharedUserInterface.SelectEntityType;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
 
 import static  org.junit.jupiter.api.Assertions.*;
 
@@ -18,6 +27,22 @@ public class SelectionCharacterTest {
     PlayerEntityFactory entitiesFactory;
     String entityName = "mockCharackter";
     String properType = "Archer";
+    String badType = "badType";
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final InputStream sysInBackup = System.in;
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setIn(sysInBackup);
+        System.setOut(originalOut);
+    }
 
     @BeforeEach
     void InitializateTest() {
@@ -55,8 +80,30 @@ public class SelectionCharacterTest {
 
     @Test
     void ShouldntCreateEntityWithWrongSelecetedType() {
-        entity = entitiesFactory.createEntity("badType", entityName, null);
+        entity = entitiesFactory.createEntity(badType, entityName, null);
 
         assertNull(entity);
+    }
+
+    @Test
+    void ShouldDiplayMessageWhenUserDontProvideNameOnFirstCharacterSelection() {
+        String message = "Provide name for player!";
+        ByteArrayInputStream in = new ByteArrayInputStream((properType + "\n" + properType + " " + entityName).getBytes());
+        System.setIn(in);
+        entity = new SelectEntityType().createNewEntity(entity, playerEntityController);
+
+        String[] output = outContent.toString().split("\n");
+        assertEquals(message, output[output.length - 1].trim());
+    }
+
+    @Test
+    void ShouldDiplayMessageWhenUserProvideBadClassName() {
+        String message = "Please type valid character class";
+        ByteArrayInputStream in = new ByteArrayInputStream((badType + " " + entityName + "\n" + properType + " " + entityName).getBytes());
+        System.setIn(in);
+        entity = new SelectEntityType().createNewEntity(entity, playerEntityController);
+
+        String[] output = outContent.toString().split("\n");
+        assertEquals(message, output[output.length - 1].trim());
     }
 }
